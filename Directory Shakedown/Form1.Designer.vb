8d27d8f738c178dc1d2acd954ef0d8eb732b4022
Greg Patton <greg.patton@hp.com>
2013-05-02 06:45:45 -0500

    initial commit
    A Windows Forms Applicaiton that performs that provides various
    directory search capabilities.  Written in VB.NET

---
+++ Directory Shakedown/Form1.Designer.vb
@@ -0,0 +1,256 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtDirectory = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cmdSearch = New System.Windows.Forms.Button()
+        Me.cmbMethod = New System.Windows.Forms.ComboBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.cmdReset = New System.Windows.Forms.Button()
+        Me.txtFilter = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.chkSub = New System.Windows.Forms.CheckBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.cmdSave = New System.Windows.Forms.Button()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label1.BackColor = System.Drawing.Color.DarkBlue
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(13, 9)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(728, 49)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Directory Shakedown"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtDirectory)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.cmdSearch)
+        Me.GroupBox1.Controls.Add(Me.cmbMethod)
+        Me.GroupBox1.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 76)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(359, 100)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Search"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(8, 48)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Method:"
+        '
+        'txtDirectory
+        '
+        Me.txtDirectory.Location = New System.Drawing.Point(79, 21)
+        Me.txtDirectory.Name = "txtDirectory"
+        Me.txtDirectory.Size = New System.Drawing.Size(274, 21)
+        Me.txtDirectory.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(7, 21)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Directory:"
+        '
+        'cmdSearch
+        '
+        Me.cmdSearch.Enabled = False
+        Me.cmdSearch.Location = New System.Drawing.Point(10, 72)
+        Me.cmdSearch.Name = "cmdSearch"
+        Me.cmdSearch.Size = New System.Drawing.Size(62, 23)
+        Me.cmdSearch.TabIndex = 4
+        Me.cmdSearch.Text = "Get List"
+        Me.cmdSearch.UseVisualStyleBackColor = True
+        '
+        'cmbMethod
+        '
+        Me.cmbMethod.FormattingEnabled = True
+        Me.cmbMethod.Items.AddRange(New Object() {"List File Extensions", "List File Names", "List Directories"})
+        Me.cmbMethod.Location = New System.Drawing.Point(79, 45)
+        Me.cmbMethod.Name = "cmbMethod"
+        Me.cmbMethod.Size = New System.Drawing.Size(274, 21)
+        Me.cmbMethod.TabIndex = 1
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.cmdReset)
+        Me.GroupBox2.Controls.Add(Me.txtFilter)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.chkSub)
+        Me.GroupBox2.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(383, 76)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(359, 100)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Options"
+        '
+        'cmdReset
+        '
+        Me.cmdReset.Location = New System.Drawing.Point(292, 45)
+        Me.cmdReset.Name = "cmdReset"
+        Me.cmdReset.Size = New System.Drawing.Size(61, 23)
+        Me.cmdReset.TabIndex = 7
+        Me.cmdReset.Text = "Reset"
+        Me.cmdReset.UseVisualStyleBackColor = True
+        '
+        'txtFilter
+        '
+        Me.txtFilter.Location = New System.Drawing.Point(49, 45)
+        Me.txtFilter.Name = "txtFilter"
+        Me.txtFilter.Size = New System.Drawing.Size(237, 21)
+        Me.txtFilter.TabIndex = 3
+        Me.txtFilter.Text = "*.*"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Filter:"
+        '
+        'chkSub
+        '
+        Me.chkSub.AutoSize = True
+        Me.chkSub.Location = New System.Drawing.Point(7, 21)
+        Me.chkSub.Name = "chkSub"
+        Me.chkSub.Size = New System.Drawing.Size(152, 17)
+        Me.chkSub.TabIndex = 2
+        Me.chkSub.Text = "Search Subdirectories"
+        Me.chkSub.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.cmdSave)
+        Me.GroupBox3.Controls.Add(Me.txtOutput)
+        Me.GroupBox3.Controls.Add(Me.Button1)
+        Me.GroupBox3.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox3.Location = New System.Drawing.Point(16, 196)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(725, 325)
+        Me.GroupBox3.TabIndex = 3
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Output"
+        '
+        'cmdSave
+        '
+        Me.cmdSave.Location = New System.Drawing.Point(11, 296)
+        Me.cmdSave.Name = "cmdSave"
+        Me.cmdSave.Size = New System.Drawing.Size(85, 23)
+        Me.cmdSave.TabIndex = 7
+        Me.cmdSave.Text = "Save to File"
+        Me.cmdSave.UseVisualStyleBackColor = True
+        '
+        'txtOutput
+        '
+        Me.txtOutput.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.txtOutput.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOutput.Location = New System.Drawing.Point(10, 21)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtOutput.Size = New System.Drawing.Size(710, 269)
+        Me.txtOutput.TabIndex = 5
+        Me.txtOutput.WordWrap = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(102, 296)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(61, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Clear"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ClientSize = New System.Drawing.Size(754, 533)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Directory Shakedown"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtFilter As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents chkSub As System.Windows.Forms.CheckBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtDirectory As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents cmdSearch As System.Windows.Forms.Button
+    Friend WithEvents cmbMethod As System.Windows.Forms.ComboBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtOutput As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents cmdReset As System.Windows.Forms.Button
+    Friend WithEvents cmdSave As System.Windows.Forms.Button
+
+End Class
---
+++ Directory Shakedown/Form1.vb
@@ -0,0 +1,164 @@
+﻿Public Class frmMain
+
+    Public Enum Methods
+        ListExtensions = 0
+        ListFiles = 1
+        ListDirectories = 2
+    End Enum
+
+
+
+    Private Sub cmbMethod_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbMethod.SelectedIndexChanged
+        Try
+            Select Case cmbMethod.SelectedIndex
+
+                Case Methods.ListFiles, Methods.ListExtensions, Methods.ListDirectories
+                    cmdSearch.Enabled = True
+                Case Else
+                    cmdSearch.Enabled = False
+            End Select
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Method Selection Error!")
+        End Try
+    End Sub
+
+    Private Sub cmdSearch_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSearch.Click
+        Try
+
+            If txtDirectory.Text = String.Empty Then
+                Dim exDirectory As New Exception("Error: Directory Not Provided")
+                Throw exDirectory
+            End If
+
+            txtOutput.Text = String.Empty
+
+            Dim DIR As IO.DirectoryInfo
+            DIR = New IO.DirectoryInfo(txtDirectory.Text)
+            Dim FI As IO.FileInfo
+            Dim SearchOutput As New Hashtable()
+
+
+            Select Case cmbMethod.SelectedIndex
+
+                Case Methods.ListExtensions
+                    If chkSub.Checked Then
+                        For Each FI In DIR.GetFiles(txtFilter.Text, IO.SearchOption.AllDirectories)
+                            If Not SearchOutput.ContainsValue(FI.Extension.ToLower()) Then
+                                SearchOutput.Add(SearchOutput.Count, FI.Extension.ToLower())
+                            End If
+                        Next
+                    Else
+                        For Each FI In DIR.GetFiles(txtFilter.Text, IO.SearchOption.TopDirectoryOnly)
+                            If Not SearchOutput.ContainsValue(FI.Extension.ToLower()) Then
+                                SearchOutput.Add(SearchOutput.Count, FI.Extension.ToLower())
+                            End If
+                        Next
+                    End If
+
+                    Dim i As Integer
+                    For i = 0 To SearchOutput.Count - 1
+                        txtOutput.Text = txtOutput.Text + CType(SearchOutput.Item(i), String) + vbCrLf
+                    Next
+
+                Case Methods.ListFiles
+                    If chkSub.Checked Then
+                        For Each FI In DIR.GetFiles(txtFilter.Text, IO.SearchOption.AllDirectories)
+                            If Not SearchOutput.ContainsValue(FI.FullName) Then
+                                SearchOutput.Add(SearchOutput.Count, FI.FullName.ToLower())
+                            End If
+                        Next
+                    Else
+                        For Each FI In DIR.GetFiles(txtFilter.Text, IO.SearchOption.TopDirectoryOnly)
+                            If Not SearchOutput.ContainsValue(FI.FullName) Then
+                                SearchOutput.Add(SearchOutput.Count, FI.FullName.ToLower())
+                            End If
+                        Next
+                    End If
+
+                    Dim i As Integer
+                    For i = 0 To SearchOutput.Count - 1
+                        txtOutput.Text = txtOutput.Text + CType(SearchOutput.Item(i), String) + vbCrLf
+                    Next
+
+                Case Methods.ListDirectories
+                    If chkSub.Checked Then
+                        For Each item In DIR.GetDirectories(txtFilter.Text, IO.SearchOption.AllDirectories)
+                            If Not SearchOutput.ContainsValue(item.FullName) Then
+                                SearchOutput.Add(SearchOutput.Count, item.FullName.ToLower())
+                            End If
+                        Next
+                    Else
+                        For Each item In DIR.GetDirectories(txtFilter.Text, IO.SearchOption.TopDirectoryOnly)
+                            If Not SearchOutput.ContainsValue(item.FullName) Then
+                                SearchOutput.Add(SearchOutput.Count, item.FullName.ToLower())
+                            End If
+                        Next
+
+                    End If
+
+                    Dim i As Integer
+                    For i = 0 To SearchOutput.Count - 1
+                        txtOutput.Text = txtOutput.Text + CType(SearchOutput.Item(i), String) + vbCrLf
+                    Next
+
+                Case Else
+                    Dim exSearch As New Exception("Invalid Search")
+                    Throw exSearch
+
+            End Select
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Search Error!")
+        End Try
+    End Sub
+
+    Private Sub cmdReset_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdReset.Click
+        Try
+            txtFilter.Text = "*.*"
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Reset Filter Error!")
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Try
+            txtOutput.Text = String.Empty
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Clear Output Error!")
+        End Try
+    End Sub
+
+    Private Sub frmMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Try
+            cmbMethod.SelectedIndex = Methods.ListExtensions
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error Loading Form")
+        End Try
+    End Sub
+
+    Private Sub cmdSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSave.Click
+        Try
+            Dim saveFileDialog1 As New SaveFileDialog
+            saveFileDialog1.Filter = "TXT File|*.txt"
+            saveFileDialog1.Title = "Save a txt File"
+            saveFileDialog1.InitialDirectory = Environment.CurrentDirectory
+            saveFileDialog1.FileName = "Shakedown_Output"
+
+            If (saveFileDialog1.ShowDialog() = Windows.Forms.DialogResult.Cancel) Then
+                Exit Sub
+            End If
+
+            Dim fileWriter As System.IO.StreamWriter
+            If (saveFileDialog1.FileName <> "") Then
+                fileWriter = My.Computer.FileSystem.OpenTextFileWriter(saveFileDialog1.FileName, False)
+                fileWriter.Write(txtOutput.Text)
+                fileWriter.Close()
+                MsgBox("File Successfully Saved!", MsgBoxStyle.OkOnly, "File Saved")
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Save File Error")
+        End Try
+    End Sub
+End Class
